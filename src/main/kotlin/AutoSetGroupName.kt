f071348df1c1f8e06ffbf1d1571e187817699e04
PigeonYuze <timothyfjxfa25@gmail.com>
2023-03-11 19:17:18 +0800

    Update version 1.2.0
    1. Add RandomText feature
    2. Rename `HOW_LONG_TO_DISTANCE` to `HOW_LONG_TO_DISTANCE_DETAIL`
    3. Add `HOW_LONG_TO_DISTANCE_SIMPLE` feature
    4. Fix `HOW_LONG_TO_DISTANCE` get result error
    5. Modified parameter judgment logic
    6. Modify the listening event method to conform to the plugin lifecycle

--- src/main/kotlin/AutoSetGroupName.kt
+++ src/main/kotlin/AutoSetGroupName.kt
@@ -1,8 +1,10 @@
 package com.pigeonyuze
 
 import com.pigeonyuze.Config.SetNameType.*
+import com.pigeonyuze.execute.RandomText
 import com.pigeonyuze.execute.SystemData
 import com.pigeonyuze.execute.SystemData.toPercentage
+import com.pigeonyuze.execute.TimeData
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.mamoe.mirai.Bot
@@ -15,20 +17,13 @@ import net.mamoe.mirai.event.events.BotOnlineEvent
 import net.mamoe.mirai.utils.info
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.*
-import java.util.Calendar.*
 import kotlin.concurrent.fixedRateTimer
-
-private val Int.toPositiveNumberOrZero: Int
-    get() {
-        return if (this >= 0) this else -this
-    }
 
 object GroupBotSuffix : KotlinPlugin(
     JvmPluginDescription(
         id = "com.pigeonyuze.group-bot-suffix",
         name = "GroupBotSuffix",
-        version = "1.1.1",
+        version = "1.2.0",
     ) {
         author("鸽子宇泽")
         info("""自动设置bot在所有群聊的群名片信息，可用设置为倒计时以及相关内容""")
@@ -38,12 +33,16 @@ object GroupBotSuffix : KotlinPlugin(
 
     override fun onEnable() {
         Config.reload()
-        logger.info { "Plugin loaded" }
-        GlobalEventChannel.subscribeAlways<BotOnlineEvent> {
+        if (Config.open == HOW_LONG_TO_DISTANCE) {
+            logger.warning("配置中存在旧的`open`设置, 使用了过时的'HOW_LONG_TO_DISTANCE'!  请及时修改！")
+        }
+        /* Mirai-Console 内的生命周期 */
+        val parentScope = GlobalEventChannel.parentScope(this)
+        parentScope.subscribeAlways<BotOnlineEvent> {
             if (botsList.contains(this.bot)) return@subscribeAlways
             botsList.add(bot)
         }
-        GlobalEventChannel.subscribeAlways<BotOfflineEvent> {
+        parentScope.subscribeAlways<BotOfflineEvent> {
             if (botsList.contains(this.bot)) botsList.remove(bot)
         }
         logger.info { "Try to start task" }
@@ -54,9 +53,9 @@ object GroupBotSuffix : KotlinPlugin(
         ) {
             launch {
                 val newSuffix = Config.separator + parseContent()
-                if (Config.allowlist.isNotEmpty() && Config.allowlist[0] != 114514L){
-                    for (groupId in Config.allowlist){
-                        for (bot in botsList){
+                if (Config.allowlist.isNotEmpty() && Config.allowlist[0] != 114514L) {
+                    for (groupId in Config.allowlist) {
+                        for (bot in botsList) {
                             var group: Group?
                             if (bot.getGroup(groupId).also { group = it } == null) continue
                             group!!.botAsMember.nameCard = bot.nick + newSuffix
@@ -76,90 +75,43 @@ object GroupBotSuffix : KotlinPlugin(
         logger.info { "Start task!" }
     }
 
-    private fun parseContent(): String{
+    private fun parseContent(): String {
         var waitForVar = false
         var ret = Config.content
         var arg = StringBuilder()
         for (char in Config.content) {
             if (char == '%') {
-                waitForVar = true
+                if (!waitForVar) {
+                    waitForVar = true
+                    continue
+                }
+                waitForVar = false
+                ret = ret.replaceFirst("%$arg%", parseContentImpl(Config.open, arg.toString()))
+                arg = arg.clear()
                 continue
             }
             if (!waitForVar) continue
             if (char == 's') {
                 waitForVar = false
-                ret = ret.replaceFirst("%s", parseContentImpl(Config.open,null))
-                arg = arg.clear()
-                continue
-            }
-            if (char == ' '){
-                waitForVar = false
-                ret = ret.replaceFirst("%$arg", parseContentImpl(Config.open,arg.toString()))
+                ret = ret.replaceFirst("%s", parseContentImpl(Config.open, null))
                 arg = arg.clear()
                 continue
             }
             arg.append(char)
         }
-        if (arg.isNotEmpty()){
-            ret = ret.replaceFirst("%$arg", parseContentImpl(Config.open,arg.toString()))
+        if (arg.isNotEmpty()) {
+            ret = ret.replaceFirst("%$arg", parseContentImpl(Config.open, arg.toString()))
         }
         logger.verbose("get-suffix: $ret")
         return ret
     }
 
-    private fun parseContentImpl(type: Config.SetNameType,arg: String?): String{
-        return when(type){
+    private fun parseContentImpl(type: Config.SetNameType, arg: String?): String {
+        return when (type) {
             NOW_TIME -> {
                 DateTimeFormatter.ofPattern(arg ?: "HH:mm:ss").format(LocalDateTime.now())
             }
-            HOW_LONG_TO_DISTANCE -> { //默认按yyyy-MM-dd HH:mm:ss解析 默认为0
-                arg!!
 
-                //region Setting
-                val now = GregorianCalendar()
-                val dateSetting = arg.substring(0,9).split("-")
-                var pattern = StringBuilder()
-
-                var year = dateSetting[0].toInt()
-                var month = dateSetting[1].toInt()
-                pattern.append("yyyy年HH月dd日")
-                var day = dateSetting.getOrNull(2)?.toInt() ?: kotlin.run{
-                    month = year
-                    year = now.get(YEAR)
-                    pattern = pattern.clear().append("HH-dd")
-                    dateSetting[1].toInt()
-                }
-                var hrs = 24
-                var min = 0
-                var sec = 0
-
-                if (arg.length > 11) { //存在详细设定
-                    pattern.append("HH小时mm分钟ss秒")
-                    val timeSetting = arg.substring(10).split(":")
-                    hrs = timeSetting.getOrNull(0)?.toInt() ?: 0
-
-                    min = timeSetting.getOrNull(1)?.toInt() ?: 0
-                    sec = timeSetting.getOrNull(2)?.toInt() ?: 0
-                }
-                //endregion
-
-                //region Minus
-                year = (year - now.get(YEAR)).toPositiveNumberOrZero
-                month = (month - now.get(MONTH)).toPositiveNumberOrZero
-                day = (day - now.get(DAY_OF_MONTH)).toPositiveNumberOrZero
-                hrs = (hrs - now.get(HOUR_OF_DAY)).toPositiveNumberOrZero
-                min = (min - now.get(MINUTE)).toPositiveNumberOrZero
-                sec = (sec - now.get(SECOND)).toPositiveNumberOrZero
-
-                pattern.toString()
-                    .replace("yyyy", year.toString())
-                    .replace("MM", month.toString())
-                    .replace("dd", day.toString())
-                    .replace("HH", hrs.toString())
-                    .replace("mm", min.toString())
-                    .replace("ss", sec.toString())
-                //endregion
-            }
             CPU_LOAD -> {
                 SystemData.systemCpuLoad.toPercentage()
             }
@@ -172,6 +124,21 @@ object GroupBotSuffix : KotlinPlugin(
             JVM_MEMORY_LOAD -> {
                 SystemData.jvmMemory.toPercentage()
             }
+            HOW_LONG_TO_DISTANCE_DETAIL -> {
+                TimeData.getHowLongToDistanceDetailImpl(arg ?: throwSuppressArgs())
+            }
+            HOW_LONG_TO_DISTANCE -> {
+                TimeData.getHowLongToDistanceImpl(arg ?: throwSuppressArgs())
+            }
+            HOW_LONG_TO_DISTANCE_SIMPLE -> {
+                TimeData.getHowLongToDistanceSimpleImpl(arg ?: throwSuppressArgs())
+            }
+            RANDOM_TEXT -> {
+                RandomText.randomTextImpl(arg ?: throwSuppressArgs())
+            }
         }
     }
+
+    private fun throwSuppressArgs(): Nothing =
+        throw IllegalArgumentException("Does not provide parameters (parameter is 's')")
 }
--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -1,21 +1,21 @@
 package com.pigeonyuze
 
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
+import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
-import net.mamoe.yamlkt.Comment
 
 object Config : AutoSavePluginConfig("Setting") {
-    @Comment("""
+    @ValueDescription("""
         每次间隔的时间 单位为毫秒
         不建议将此项设得过快 因为mirai并不主动推送群bot名片修改的事件(用户查询bot群名片/发送信息时才可能修改) 过快并不一定有效
     """)
     val waitTimeMS: Long by value(60_000L)
 
-    @Comment(
+    @ValueDescription(
         """
         选择的方法，可选以下内容 
         NOW_TIME (现在的时间 可提供参数自行设置 默认为HH:mm:ss)
-        HOW_LONG_TO_DISTANCE (距离什么时候还有多久 需要用参数提供指定日期(yyyy-MM-dd HH:mm:ss) 默认HH:mm:ss会设置为0:0:0),
+        HOW_LONG_TO_DISTANCE_DETAIL (距离什么时候还有多久 需要用参数提供指定日期(yyyy-MM-dd HH:mm:ss) 默认HH:mm:ss会设置为0:0:0),
         CPU_LOAD (系统cpu占用率),
         JVM_CPU_LOAD (jvm可使用的cpu占用率),
         MEMORY_LOAD (系统内存占用率),
@@ -24,36 +24,40 @@ object Config : AutoSavePluginConfig("Setting") {
     )
     val open: SetNameType by value(SetNameType.NOW_TIME)
 
-    @Comment("""
+    @ValueDescription("""
         设置的名片后缀 使用%xxx来代替对应的值
         使用%s为默认参数
         也可用%xxx ('%'后面为参数 直到空格结束)
     """)
     val content: String by value("现在是北京时间 %s !")
 
-    @Comment("""
+    @ValueDescription("""
         bot名称与后缀的分割符号
     """)
     val separator: String by value(" | ")
 
-    @Comment(
+    @ValueDescription(
         """
             经过多少毫秒后修改下一个群聊的群名片
             (如果为0的话就不会等待)
         """)
     val waitGroupMS: Long by value(100L)
 
-    @Comment("""
+    @ValueDescription("""
         只在以下群聊开启
     """)
     val allowlist: List<Long> by value(listOf(114514L))
 
     enum class SetNameType{
         NOW_TIME,
+        HOW_LONG_TO_DISTANCE_DETAIL,
+        /* 向下兼容，保存时进行切换 */
         HOW_LONG_TO_DISTANCE,
+        HOW_LONG_TO_DISTANCE_SIMPLE,
         CPU_LOAD,
         JVM_CPU_LOAD,
         MEMORY_LOAD,
-        JVM_MEMORY_LOAD;
+        JVM_MEMORY_LOAD,
+        RANDOM_TEXT;
     }
 }
